729f5322e98bf153fbf995b053bdf8348eddf988
Muhammad Salman <salmannawaz008@gmail.com>
2025-06-17 23:18:04 +0500

    fixed width issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
-import Header from './Header'
-import Footer from './Footer'
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
@@ -8,5 +8,5 @@ export default function Layout({ children }) {
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
